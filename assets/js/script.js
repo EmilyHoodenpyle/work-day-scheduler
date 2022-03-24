ef269adc83cb772fe7f4011fff56d67c223e7cf9
Emily Hoodenpyle <emilyghoodenpyle@gmail.com>
2022-03-23 23:14:21 -0700

    added date to jumbotron

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,30 @@
-var today = moment().format(h);
+var today = moment();
 var time = moment();
 
-if (time < today) {
-  colorPast;
-} else if (time > today) {
-  colorFuture;
-} else if (time === today) {
-  colorPresent;
+// important DOM elements
+var currentDayEl = today.format("dddd[,] MMMM Do");
+$("#currentDay").text(currentDayEl);
+
+var containerEl = $('#container');
+var tableEl = $("#row");
+
+function printHourSchedule(hour, eventText, saveBtn) {
+  var 
 }
+
+// function getCurrent(){
+//   if (time < today) {
+//     colorPast;
+//   } else if (time > today) {
+//     colorFuture;
+//   } else if (time === today) {
+//     c
+// }
+
+// if (time < today) {
+//   colorPast;
+// } else if (time > today) {
+//   colorFuture;
+// } else if (time === today) {
+//   colorPresent;
+// }
